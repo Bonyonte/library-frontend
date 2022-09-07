ee594e54c8553aa24773e05a3807bd5e80f6a938
Bonyonte <godfrey.magogo@student.moringaschool.comgit config --global user.name Bonyontegit branch -m maingit branch -M maingit remote add origin git@github.com:Bonyonte/ip_1.gitgit push -u origin maingit config --global user.email godfrey.magogo@student.moringaschool.com>
2022-09-07 11:13:34 +0300

    add Library component

---
+++ src/components/Library.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect} from 'react';
+import '../App';
+import Book from './Book';
+import axios from 'axios';
+
+const Library = () => {
+
+    const [books, setBooks] = useState([]);
+    const [ search, setSearch] = useState(" ");
+
+    // useEffect(() => {
+    //     fetch('http://localhost:3000/Books')
+    //     .then(r =>r.json())
+    //     .then((data) => setBooks(data))
+        
+    // },[])
+
+    const searchBook = (e) =>{
+      if(e.key==="Enter"){
+        axios.get("https://afternoon-sierra-69142.herokuapp.com/Books")
+        .then(res =>setBooks(res.data))
+        //.catch(err => console.log(err));
+      }
+    }
+
+    const handleOnSearch =(e) => {
+      setSearch(e.target.value)
+    }
+
+      const filterBooks = books.filter((book) => book.title.includes(search))
+    //console.log(books);
+
+      const titles = books.map((book) => book.title)
+  return (
+    <>
+    <div className="row2">
+            <h2>Find your book</h2>
+            <div className="search">
+                <input type="text" placeholder="Enter Your Book Name" 
+               value={search} onChange={handleOnSearch}
+               onKeyPress={searchBook}
+               />
+            </div>
+        </div>
+    <div className="book-list">{filterBooks.map((book) =>{
+      return <Book title={book.title} 
+      author={book.author} 
+      image={book.imgUrl} 
+      price={book.price} 
+      review={book.review} 
+      description={book.description} 
+      source={book.source} key={book.id}/>
+    })}
+    </div>
+    </>
+  )
+}
+
+export default Library;
