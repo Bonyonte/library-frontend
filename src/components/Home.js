1731c94aa808c6d3bde93ee5f1d5b7e086885b64
Bonyonte <godfrey.magogo@student.moringaschool.comgit config --global user.name Bonyontegit branch -m maingit branch -M maingit remote add origin git@github.com:Bonyonte/ip_1.gitgit push -u origin maingit config --global user.email godfrey.magogo@student.moringaschool.com>
2022-09-07 11:03:09 +0300

    add Home component and its functionality

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../App';
+
+
+const Home = () => {
+  return (
+    <div className="book-details">
+      <div className="row1"> 
+        <h1>Ordinary people have big TVs. <br/> Extraordinary people have big libraries</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Home
