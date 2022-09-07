d55c71c0000dbf44478a67e3e2cb87741571d9e2
Bonyonte <godfrey.magogo@student.moringaschool.comgit config --global user.name Bonyontegit branch -m maingit branch -M maingit remote add origin git@github.com:Bonyonte/ip_1.gitgit push -u origin maingit config --global user.email godfrey.magogo@student.moringaschool.com>
2022-09-07 11:05:29 +0300

    add Home component to app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import './App.css';
 import  { Routes, Route} from 'react-router-dom';
-// import Home from './components/Home';
+import Home from './components/Home';
 // import Library from './components/Library';
 import NavBar from './components/NavBar';
 // import Footer from './components/Footer';
