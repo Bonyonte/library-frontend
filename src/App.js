6e42a9403428265674a1fbd35748bfb19c26d915
Bonyonte <godfrey.magogo@student.moringaschool.comgit config --global user.name Bonyontegit branch -m maingit branch -M maingit remote add origin git@github.com:Bonyonte/ip_1.gitgit push -u origin maingit config --global user.email godfrey.magogo@student.moringaschool.com>
2022-09-07 10:29:49 +0300

    adding NAvBar component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from "react";
+// import './App.css';
+// import  { Routes, Route} from 'react-router-dom';
+// import Home from './components/Home';
+// import Library from './components/Library';
+// import NavBar from './components/NavBar';
+// import Footer from './components/Footer';
+// import Comment from './components/Comment';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <NavBar /> 
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/library" element={<Library />} />
+//         <Route path="/comment" element={<Comment />} />
+//       </Routes>
+//       <Footer />
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
+
