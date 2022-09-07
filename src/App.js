fb25669e639b857ae5654c63382bceeb036abf56
Bonyonte <godfrey.magogo@student.moringaschool.comgit config --global user.name Bonyontegit branch -m maingit branch -M maingit remote add origin git@github.com:Bonyonte/ip_1.gitgit push -u origin maingit config --global user.email godfrey.magogo@student.moringaschool.com>
2022-09-07 10:33:59 +0300

    calling components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-// import React from "react";
-// import './App.css';
-// import  { Routes, Route} from 'react-router-dom';
+import React from "react";
+import './App.css';
+import  { Routes, Route} from 'react-router-dom';
 // import Home from './components/Home';
 // import Library from './components/Library';
-// import NavBar from './components/NavBar';
+import NavBar from './components/NavBar';
 // import Footer from './components/Footer';
-// import Comment from './components/Comment';
+import Comment from './components/Comment';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <NavBar /> 
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/library" element={<Library />} />
-//         <Route path="/comment" element={<Comment />} />
-//       </Routes>
-//       <Footer />
-//     </div>
-//   );
-// }
+function App() {
+  return (
+    <div className="App">
+      <NavBar /> 
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/library" element={<Library />} />
+        <Route path="/comment" element={<Comment />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
 
-// export default App;
+export default App;
 
